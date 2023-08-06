5b55c357482eeadcca0ac818cb686cbe999aba8c
Nagendra Patel <nagendra.k.patel@gsk.com>
2023-08-06 17:15:19 +0530

    Initial commit

---
+++ AdditionFunctions/AdditionFunctions.h
@@ -0,0 +1,18 @@
+//
+//  AdditionFunctions.h
+//  AdditionFunctions
+//
+//  Created by Nagendra on 06/08/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AdditionFunctions.
+FOUNDATION_EXPORT double AdditionFunctionsVersionNumber;
+
+//! Project version string for AdditionFunctions.
+FOUNDATION_EXPORT const unsigned char AdditionFunctionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AdditionFunctions/PublicHeader.h>
+
+
---
+++ AdditionFunctions/AdditionFunctions.swift
@@ -0,0 +1,19 @@
+//
+//  AdditionFunctions.swift
+//  AdditionFunctions
+//
+//  Created by Nagendra on 06/08/23.
+//
+
+import Foundation
+public class AdditionFunctions {
+    public static func add(_ a: Int, _ b: Int) -> Int {
+        return a + b
+    }
+}
+
+public class ReductionFunctions {
+    public static func add(_ a: Int, _ b: Int) -> Int {
+        return a - b
+    }
+}
